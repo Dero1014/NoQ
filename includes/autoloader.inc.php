9b9196aeb8129271205e036aebc253cfd2b70a39
Dero1014 <petar.mami@gmail.com>
2022-08-09 18:22:55 +0000

    Autoloader Autoloader of includes and classes

---
+++ includes/autoloader.inc.php
@@ -0,0 +1,44 @@
+<?php
+spl_autoload_register('loadClasses');
+spl_autoload_register('loadIncludes');
+
+function loadClasses($className)
+{
+    $url = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+
+    if (strpos($url, 'includes')) {
+        $path = "../classes/";
+    }else {
+        $path = "classes/";
+    }
+
+    if (strpos($url, 'sites')) {
+        $path = "../classes/";
+    }else {
+        $path = "classes/";
+    }
+    
+    $ext = ".class.php";
+    $fullPath = $path . $className . $ext;
+    include_once $fullPath;
+}
+
+function loadIncludes($includesName)
+{
+    $url = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
+
+    if (strpos($url, 'classes')) {
+        $path = "../includes/";
+    }else {
+        $path = "includes/";
+    }
+
+    if (strpos($url, 'sites')) {
+        $path = "../includes/";
+    }else {
+        $path = "includes/";
+    }
+    $ext = ".inc.php";
+    $fullPath = $path . $includesName . $ext;
+    include_once $fullPath;
+}
