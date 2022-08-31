0be7009bb28f2b1f645132695c36a0c113d41f2f
Dero1014 <petar.mami@gmail.com>
2022-08-31 17:18:53 +0000

    Refactoring 9.1 resolved back

--- includes/user.inf.php
+++ includes/user.inf.php
@@ -1,40 +1,14 @@
 <?php
 //header('Content-type: text/plain');
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
 
 // Everything commented is what we are trying to replace
 $pathAuto = "autoloader.inc.php";
-$pathUser = "user.class.php";
-$pathSQL = "sql.class.php";
-$pathError = "errorinfo.class.php";
 $var = getcwd();
 
-if (strpos($var, 'sites')) {
+if (strpos($var, 'sites')) 
     $pathAuto = "../includes/" . $pathAuto;
-    $pathUser = "../classes/" . $pathUser;
-    $pathSQL = "../classes/" . $pathSQL;
-    $pathError = "../classes/" . $pathError;
-}else if (strpos($var, 'includes')) {
-    $pathUser = "../classes/" . $pathUser;
-    $pathSQL = "../classes/" . $pathSQL;
-    $pathError = "../classes/" . $pathError;
-}else {
-    $pathUser = "classes/" . $pathUser;
-    $pathSQL = "classes/" . $pathSQL;
-    $pathError = "classes/" . $pathError;
-}
-//echo "$pathUser ";
-//echo "$pathSQL ";
-//echo "$pathError ";
 
 include_once $pathAuto;
-//include_once $pathError;
-//include_once $pathSQL;
-//include_once $pathUser;
-
-//$user = new User(-1, 'template', 'template', 0);
 
 session_start();
 
